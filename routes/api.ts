99799046cc3dba86b578c5333ee2f167c7444791
Juhyung Park <jh@kodebox.io>
2018-08-17 19:07:00 +0900

    Add api paths

--- routes/api.ts
+++ routes/api.ts
@@ -4,9 +4,39 @@ import { Context } from "../context";
 export function createRouter(context: Context) {
     const router = express.Router();
 
-    router.post("/hi", async (req, res) => {
+    router.get("/keys", async (req, res) => {
         res.json({
-            message: "HI"
+            message: "NotImplementedYet"
+        });
+    });
+
+    router.post("/keys", async (req, res) => {
+        res.json({
+            message: "NotImplementedYet"
+        });
+    });
+
+    router.post("/key/:key/remove", async (req, res) => {
+        res.json({
+            message: "NotImplementedYet"
+        });
+    });
+
+    router.post("/key/:key/sign", async (req, res) => {
+        res.json({
+            message: "NotImplementedYet"
+        });
+    });
+
+    router.post("/pubkeyhashes", async (req, res) => {
+        res.json({
+            message: "NotImplementedYet"
+        });
+    });
+
+    router.get("/pubkeyhashes/:hash", async (req, res) => {
+        res.json({
+            message: "NotImplementedYet"
         });
     });
 
