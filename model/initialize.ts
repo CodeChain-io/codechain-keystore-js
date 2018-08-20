9779b4a4fc61911bccc3d2f7961f99d630e79221
Juhyung Park <jh@kodebox.io>
2018-08-20 09:08:21 +0900

    Add keys and pkhs table

--- model/initialize.ts
+++ model/initialize.ts
@@ -4,7 +4,16 @@ import { asyncRun } from "./util";
 export async function initialize(db: sqlite3.Database): Promise<void> {
     // TODO: need index in createdAt and url
     await asyncRun(db,
-        `CREATE TABLE IF NOT EXISTS hi (
-id INTEGER PRIMARY KEY ASC
+        `CREATE TABLE IF NOT EXISTS keys (
+id INTEGER PRIMARY KEY ASC,
+encryptedPrivateKey TEXT,
+publicKey TEXT
+)`, {});
+
+    await asyncRun(db,
+    `CREATE TABLE IF NOT EXISTS pkhs (
+id INTEGER PRIMARY KEY ASC,
+hash TEXT,
+publicKey TEXT
 )`, {});
 }
